0ac48ab9895574b64dae2bbc8ca3cca883830008
Jens Fischer <JensFZ@gmail.com>
2023-05-09 22:11:02 +0200

    first commit

---
+++ lib/todos.ex
@@ -0,0 +1,10 @@
+defmodule Todos do
+  def create_todos do
+    # io.gets liest ein string ein
+    anzahl_der_todos = IO.gets("Anzahl der Todos: ")
+
+    # Integer.parse zerlegt die eingabe in integer und string -> wird gleich den beiden variablen zugewiesen
+    {anzahl_der_todos, _rest} = Integer.parse(anzahl_der_todos)
+    anzahl_der_todos
+  end
+end

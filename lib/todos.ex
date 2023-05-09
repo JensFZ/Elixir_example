2f37fe0b0ff85c97619bf3a4e07945f701ed7f9b
Jens Fischer <JensFZ@gmail.com>
2023-05-09 22:26:17 +0200

    rewrite mit pipes

--- lib/todos.ex
+++ lib/todos.ex
@@ -1,10 +1,18 @@
 defmodule Todos do
   def create_todos do
-    # io.gets liest ein string ein
-    anzahl_der_todos = IO.gets("Anzahl der Todos: ")
+    # # alter code - messy ;)
+    # # io.gets liest ein string ein
+    # anzahl_der_todos = IO.gets("Anzahl der Todos: ")
 
-    # Integer.parse zerlegt die eingabe in integer und string -> wird gleich den beiden variablen zugewiesen
-    {anzahl_der_todos, _rest} = Integer.parse(anzahl_der_todos)
+    # # Integer.parse zerlegt die eingabe in integer und string -> wird gleich den beiden variablen zugewiesen
+    # {anzahl_der_todos, _rest} = Integer.parse(anzahl_der_todos)
+
+    # # ausgabe
+    # anzahl_der_todos
+
+    # neuer code - pipe for the win ;)
+
+    {anzahl_der_todos, _} = IO.gets("Anzahl der Todos: ") |> Integer.parse()
 
     # ausgabe
     anzahl_der_todos
